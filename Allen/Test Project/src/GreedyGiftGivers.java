303a9a8ffa939f0d9559310885f4d45c5c1e9de8
Allen <allenlu378@gmail.com>
2017-01-22 23:04:36 -0500

    Greedy Gift Givers

---
+++ Allen/Test Project/src/GreedyGiftGivers.java
@@ -0,0 +1,16 @@
+/*
+ID: allenlu2
+LANG: JAVA
+TASK: gift1
+*/
+
+import java.io.*;
+import java.util.*;
+public class GreedyGiftGivers {
+	public static void main(String args[]) throws IOException
+	{
+		BufferedReader f = new BufferedReader(new FileReader("test.in"));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("test.out")));
+		StringTokenizer st = new StringTokenizer(f.readLine());
+	}
+}
